75007677a4770d59a8fc5306d8e108181167aca3
amwang <undefined>
2023-08-25 20:45:07 +0000

    initial

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include "mbed.h"
+
 // Cylon style LED scanner
-BusOut led_state(LED1, LED2, LED3, LED4);
+// BusOut led_state(LED1, LED2, LED3, LED4);
+
+DigitalIn enable(p26);
+DigitalOut led(p23);
+
+DigitalOut debug(LED1);
 
 int main() {
-    int i = 0;
-    // initinal LED state
-    led_state = 0x1;
-    while (1) {
-        // loop through all states
-        for (i=0; i<6; i++) {
-            if (i<3)
-                // shift left 1 bit until high LED set
-                led_state = led_state << 1;
-            else
-                // then reverse and shift back to right 1 bit
-                led_state = led_state >> 1;
-            // time delay .1s to slow down display
-            wait(0.1);
+     while(1) {
+        debug = 0;
+
+        if (enable) { // button released
+            debug = 1;
+            wait(0.2);
+            debug = 0;
+            wait(0.2);
+
+            led = 0;
+            wait(0.2);
+        } else {
+            led = 1;
+            wait(0.2);
         }
     }
 }
