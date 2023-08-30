12f3ff40bce355878e0c5c0d610a05d9dc96516f
amwnag <amwang36@gmail.com>
2023-08-30 19:02:15 +0000

    initial initial

--- main.cpp
+++ main.cpp
@@ -1,28 +1,17 @@
 #include "mbed.h"
 
-// Cylon style LED scanner
-// BusOut led_state(LED1, LED2, LED3, LED4);
+DigitalIn button(p22);
+DigitalOut red(p21);
 
-DigitalIn enable(p26);
-DigitalOut led(p23);
-
-DigitalOut debug(LED1);
 
 int main() {
-     while(1) {
-        debug = 0;
-
-        if (enable) { // button released
-            debug = 1;
-            wait(0.2);
-            debug = 0;
-            wait(0.2);
-
-            led = 0;
-            wait(0.2);
+    red = 0;
+    while(1) {
+        if (button) { // not pressed
+            red = 0;
         } else {
-            led = 1;
-            wait(0.2);
+            red = 1;
         }
+        wait(0.2);
     }
 }
